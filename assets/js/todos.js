9cdc76fffaa2100fc736235c285956509cac89c9
Ting Lin <timlin.mail@gmail.com>
2018-09-11 10:52:45 -0700

    comments added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -7,16 +7,21 @@ $("ul").on("click", "li", function(){
 $("ul").on("click", "span", function(event){
 	// using call back function to avoid the immediate remove
 	$(this).parent().fadeOut(500, function(){
+		// this here is li = span.parent()
 		$(this).remove();
 	});
 	// to avoid the propagation to its parent element
+	// the even bubbling is going from specific to general
 	event.stopPropagation();
 });
 
+// control input text to check if "enter has been hit"
 $("input[type='text']").keypress(function(event){
+	// event key code 13 is enter
 	if(event.which === 13){
 		// grab input 
 		var todoText = $(this).val();
+		// remove the input 
 		$(this).val("");
 		// create new li and add to ul
 		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
